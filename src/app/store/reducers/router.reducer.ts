c74aa6722a4941a9c45f90e7e8c2722f12051118
KAL99-byte <danielkalasa99@gmail.com>
2024-09-11 23:18:09 +0300

    Angular version 10 updates

--- src/app/store/reducers/router.reducer.ts
+++ src/app/store/reducers/router.reducer.ts
@@ -5,6 +5,7 @@ import {
   RouterReducerState,
   RouterStateSerializer
 } from '@ngrx/router-store';
+import { Injectable } from "@angular/core";
 
 export interface RouterStateUrl {
   url: string;
@@ -16,6 +17,7 @@ export interface State {
   router: RouterReducerState<RouterStateUrl>;
 }
 
+@Injectable()
 export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
   serialize(routerState: RouterStateSnapshot): RouterStateUrl {
     let route = routerState.root;
