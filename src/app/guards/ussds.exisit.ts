f5e0a1bededacdbdcb6fd52035144faa3e265ed9
KAL99-byte <danielkalasa99@gmail.com>
2024-09-12 00:48:39 +0300

    angularv16

--- src/app/guards/ussds.exisit.ts
+++ src/app/guards/ussds.exisit.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot } from '@angular/router';
 
 import { Store } from '@ngrx/store';
 
@@ -10,7 +10,7 @@ import {selectUssdLoaded} from '../store/selectors/ussd.selectors';
 import {ApplicationState} from '../store/reducers/index';
 
 @Injectable()
-export class UssdExistsGuards implements CanActivate {
+export class UssdExistsGuards  {
   constructor(private store: Store<ApplicationState>) {}
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
