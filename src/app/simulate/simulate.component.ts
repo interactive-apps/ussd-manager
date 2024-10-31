6b29b886e557e70eada0ade6822c77f27ac95251
KAL99-byte <danielkalasa99@gmail.com>
2024-10-31 11:32:43 +0300

    Add mobile simulator interface

--- src/app/simulate/simulate.component.ts
+++ src/app/simulate/simulate.component.ts
@@ -1,10 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild
-} from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { UssdService } from "../shared/services/ussd.service";
 import { ActivatedRoute } from "@angular/router";
@@ -15,20 +9,19 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ["./simulate.component.css"]
 })
 export class SimulateComponent implements OnInit, AfterViewInit {
+  isStart: boolean = true;
   phone = null;
   phoneFormatError = "";
-  // url = "https://pimacovid-dev.moh.go.tz";
-  url = "http://localhost:3001"
+  url = "https://etl.dhis2.udsm.ac.tz";
   sessionId = "";
   id;
-  //@ViewChild('input1') inputEl: ElementRef;
-
   need_input = false;
   sending_response = false;
   response_failed = false;
   response_ready = false;
   response_body = "";
   answer = "";
+  isImageLoaded = false;
 
   constructor(
     private http: HttpClient,
@@ -37,14 +30,18 @@ export class SimulateComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit() {
+    this.isStart = true;
     this.id = this.route.snapshot.paramMap.get("id");
   }
 
-  ngAfterViewInit() {
-    //this.inputEl.nativeElement.focus();
+  ngAfterViewInit() {}
+
+  onImageLoad() {
+    this.isImageLoaded = true;
   }
 
   cancelRequest() {
+    this.isStart = true;
     this.http.get(this.getUrl("*152*05*01", "UC")).subscribe(data => {
       this.response_body = "";
       this.response_ready = false;
@@ -68,12 +65,11 @@ export class SimulateComponent implements OnInit, AfterViewInit {
           if (d.options) {
             this.response_body = "";
             console.log("data", d);
-            this.response_body += `${d.text}.<br/><ul>`;
-            //console.log("Object.keys(d.options)", Object.keys(d.options));
+            this.response_body += `${d.text}.<p>`;
             Object.keys(d.options).forEach(key => {
-              this.response_body += `<li>${key}. ${d.options[key]}</li>`;
+              this.response_body += `${key} ${d.options[key]}<br/>`;
             });
-            this.response_body += "</ul>";
+            this.response_body += "</p>";
             console.log(this.response_body);
           } else {
             this.response_body = d.text.replace(
@@ -98,20 +94,18 @@ export class SimulateComponent implements OnInit, AfterViewInit {
   }
 
   sendFirstRequest() {
-    //check number format
-
     if (/^\d+$/.test(this.phone) && this.phone.length == 12) {
       this.phoneFormatError = "";
       this.sending_response = true;
       this.response_ready = false;
       this.sessionId = this.ussdService.make_session_id();
       console.log("URL:", this.getUrl("*152*05*01", "NR"));
-
       this.http
         .get(this.getUrl("*152*05*01", "NR"), { responseType: "text" })
         .subscribe(
           (data: any) => {
-            console.log("data response", data);
+            this.isStart = false;
+            console.log("data response data.....", data);
             console.log(data, typeof data);
             try {
               let d = JSON.parse(data);
@@ -121,12 +115,12 @@ export class SimulateComponent implements OnInit, AfterViewInit {
               console.log("Body:", d);
               this.response_body = d.text;
               if (d.options) {
-                this.response_body += "<ul>";
+                this.response_body += "<p>";
                 console.log("Object.keys(d.options)", Object.keys(d.options));
                 Object.keys(d.options).forEach(key => {
-                  this.response_body += `<li>${key}. ${d.options[key]}</li>`;
+                  this.response_body += `${key} ${d.options[key]}<br/>`;
                 });
-                this.response_body += "</ul>";
+                this.response_body += "</p>";
                 console.log(this.response_body);
               }
             } catch (e) {
